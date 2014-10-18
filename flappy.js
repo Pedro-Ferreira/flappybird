9064239166ea64a51d08fe8c1b98b122574d0c43
Pedro Ferreira <pedro.d.c.ferreira@gmail.com>
2014-10-18 15:44:39 +0100

    comments added

--- flappy.js
+++ flappy.js
@@ -38,7 +38,7 @@ function create() {
     player = game.add.sprite(50, 50, "playerImg")// loads an image
     game.add.audio("score")// loads sounds
 
-    game.input.onDown.add(clickHandler);
+    game.input.onDown.add(clickHandler);// uses keyboard arrows to move sprite
     var right_key = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     right_key.onDown.add(moveRight);
     var left_key = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -50,12 +50,12 @@ function create() {
 
 
 
-    for (var count1 = 0; count1 < 8; count1++){
-    var hole = Math.floor(Math.random() * 5) +1;
+    for (var count1 = 0; count1 < 8; count1++){ //creates a pipe with 2 holes
+    var hole = Math.floor(Math.random() * 5) +1;//randomises where the hole is
     for (var count = 0; count < hole; count = count + 1) {
         game.add.sprite(200 * count1, 50 * count, "pipe");
     }
-    for (var count = hole+2; count < 8; count = count + 1) {
+    for (var count = hole+2; count < 8; count = count + 1) {//makes the 2 holes
         game.add.sprite(200 * count1, 50 * count, "pipe");
     }
     }
@@ -88,14 +88,14 @@ function clickHandler(mouse) {
     player.y = mouse.y;
 }
 function moveRight(){
-    player.x = player.x + 10;
+    player.x = player.x + 10; //moves the cursor right
 }
 function moveLeft(){
-    player.x = player.x - 10;
+    player.x = player.x - 10; //moves the cursor left
 }
 function moveUp(){
-    player.y = player.y - 10;
+    player.y = player.y - 10;  //moves the cursor up
 }
 function moveDown(){
-    player.y = player.y + 10;
+    player.y = player.y + 10;  //moves the cursor down
 }
